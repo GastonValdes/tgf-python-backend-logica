5f80545d99280b18ff17019af5f0223a349d0e05
Gaston Valdes <gastonvaldes@gmail.com>
2020-10-22 21:35:50 -0300

    arregle la logica, estaba llena de errores

--- main.py
+++ main.py
@@ -2,18 +2,23 @@ import requests
 from flask import Flask, jsonify 
 from flask_cors import CORS
 
-
 app = Flask(__name__)
-
-#cors = CORS(app, resources={r"/calculos": {"origins": "http://localhost:port"}})
 CORS(app)
+#CORS(app, origins=['127.0.0.1:4000'])
+
+#cors = CORS(app, resources={r"/calculos": {"origins": "http://localhost:4000"}})
+
 @app.route('/', methods=["GET"])
 def execute():
-    return {'Respuesta': 'API REST Desarrollada por Gaston Valdos para el Trabajo Final de Graduacion de Licenciatura en Informatica (2020) - Esta API cumple la funcion de ejecutar la logica de control del sistema'},200
+    return {'Respuesta': 'API REST Desarrollada por Gaston Valdes para el Trabajo Final de Graduacion de Licenciatura en Informatica (2020) - Esta API cumple la funcion de ejecutar la logica de control del sistema'},200
 
 @app.route('/calculos', methods=["GET"])
 
 def executeCalc():
+
+    #urlSensores = 'http://172.18.0.3:3010/sensores'
+    #urlUmbrales = 'http://172.18.0.3:3010/umbrales'
+    #urlActuadores = 'http://172.18.0.3:3010/actuadores'
 
     urlSensores = 'http://localhost:3010/sensores'
     urlUmbrales = 'http://localhost:3010/umbrales'
@@ -54,8 +59,9 @@ def executeCalc():
     if rSensor_temp_int.status_code == 200: # Chequeo si el resultado de la API fue correcto
         rSensor_temp_int_json = rSensor_temp_int.json() # paso el resultado a JSON
         rSensor_temp_int_Medicion = rSensor_temp_int_json['medicion'] #Obtengo el valor de la propiedad Medicion 
-        print('medicion Sensor Temperatura Interior')
-        print(rSensor_temp_int_Medicion)
+        print('medicion Sensor Temperatura Interior: ' +str(rSensor_temp_int_Medicion)) 
+    else:
+        print ('la lectura de temp interior devuelve error: '+str(rSensor_temp_int.status_code))
     ################################################################
     
 
@@ -65,8 +71,9 @@ def executeCalc():
     if rSensor_temp_ext.status_code == 200: # Chequeo si el resultado de la API fue correcto
         rSensor_temp_ext_json = rSensor_temp_ext.json() # paso el resultado a JSON
         rSensor_temp_ext_Medicion = rSensor_temp_ext_json['medicion'] #Obtengo el valor de la propiedad Medicion 
-        print('medicion Sensor Temperatura Exterior')
-        print(rSensor_temp_ext_Medicion)
+        print('medicion Sensor Temperatura Exterior: '+str(rSensor_temp_ext_Medicion))
+    else:
+        print ('la lectura de temp exterior devuelve error: '+str(rSensor_temp_ext.status_code))
     ###############################################################
 
     ###############################################################
@@ -75,8 +82,9 @@ def executeCalc():
     if rSensor_ilum_int.status_code == 200: # Chequeo si el resultado de la API fue correcto
         rSensor_ilum_int_json = rSensor_ilum_int.json() # paso el resultado a JSON
         rSensor_ilum_int_Medicion = rSensor_ilum_int_json['medicion'] #Obtengo el valor de la propiedad Medicion 
-        print('medicion Sensor Iluminacion Interior')
-        print(rSensor_ilum_int_Medicion)
+        print('medicion Sensor Iluminacion Interior '+str(rSensor_ilum_int_Medicion)) 
+    else:
+        print ('la lectura de iluminacion interior devuelve error: '+ str(rSensor_ilum_int.status_code)) 
     ################################################################
 
     ###############################################################
@@ -85,8 +93,9 @@ def executeCalc():
     if rSensor_ilum_ext.status_code == 200: # Chequeo si el resultado de la API fue correcto
         rSensor_ilum_ext_json = rSensor_ilum_ext.json() # paso el resultado a JSON
         rSensor_ilum_ext_Medicion = rSensor_ilum_ext_json['medicion'] #Obtengo el valor de la propiedad Medicion 
-        print('medicion Sensor Iluminacion Exterior')
-        print(rSensor_ilum_ext_Medicion)
+        print('medicion Sensor Iluminacion Exterior: '+str(rSensor_ilum_ext_Medicion))
+    else:
+        print ('la lectura de iluminacion exterior devuelve error: '+str(rSensor_ilum_ext.status_code))
     ################################################################    
     
     ###############################################################
@@ -95,31 +104,33 @@ def executeCalc():
     if rSensor_pres_int.status_code == 200: # Chequeo si el resultado de la API fue correcto
         rSensor_pres_int_json = rSensor_pres_int.json() # paso el resultado a JSON
         rSensor_pres_int_Medicion = rSensor_pres_int_json['medicion'] #Obtengo el valor de la propiedad Medicion 
-        print('medicion Sensor Presencia Interior')
-        print(rSensor_pres_int_Medicion)
-        
+        print('medicion Sensor Presencia Interior: ' +str(rSensor_pres_int_Medicion))
+    else:
+        print ('la lectura de presencia devuelve error: '+str(rSensor_pres_int.status_code))
     ################################################################    
 
     ###############################################################
     ###       Este bloque trae el umbral de Temperatura         ###
     #print (urlUmbrales + '/' + umbr_temp)
-    rUmbral_temp = requests.get(urlUmbrales + '/' + umbr_temp)  # Armo la llamada a la API para el sensor de temp interior
+    rUmbral_temp = requests.get(urlUmbrales + '/' + umbr_temp)  # Armo la llamada a la API para el umbral de temp
     if rUmbral_temp.status_code == 200: # Chequeo si el resultado de la API fue correcto
         rUmbral_temp_json = rUmbral_temp.json() # paso el resultado a JSON
         rUmbral_Temp_Medicion = rUmbral_temp_json['valor'] #Obtengo el valor de la propiedad Medicion 
-        print('valor Umbral de Temperatura')
-        print(rUmbral_Temp_Medicion)
+        print('valor Umbral de Temperatura: '+str(rUmbral_Temp_Medicion))
+    else:
+        print ('la lectura de umbral de temperatura devuelve error: '+str(rUmbral_temp.status_code))
     ################################################################
     
     ###############################################################
     ###       Este bloque trae el umbral de Temperatura         ###
     #print (urlUmbrales + '/' + umbr_ilum)
-    rUmbral_ilum = requests.get(urlUmbrales + '/' + umbr_ilum)  # Armo la llamada a la API para el sensor de temp interior
-    if rUmbral_temp.status_code == 200: # Chequeo si el resultado de la API fue correcto
+    rUmbral_ilum = requests.get(urlUmbrales + '/' + umbr_ilum)  # Armo la llamada a la API para el umblra de iluminacion
+    if rUmbral_ilum.status_code == 200: # Chequeo si el resultado de la API fue correcto
         rUmbral_ilum_json = rUmbral_ilum.json() # paso el resultado a JSON
         rUmbral_ilum_Medicion = rUmbral_ilum_json['valor'] #Obtengo el valor de la propiedad Medicion 
-        print('valor Umbral de Iluminacion')
-        print(rUmbral_ilum_Medicion)
+        print('valor Umbral de Iluminacion: '+str(rUmbral_ilum_Medicion))
+    else:
+        print ('la lectura de umbral de temperatura devuelve error: '+str(rUmbral_ilum.status_code))
     #####################################################################
 
     #####################################################################
@@ -129,9 +140,10 @@ def executeCalc():
     if rAct_temp.status_code == 200: # Chequeo si el resultado de la API fue correcto
         rAct_temp_json = rAct_temp.json() # paso el resultado a JSON
         rAct_temp_Medicion = rAct_temp_json['operacion'] #Obtengo el valor de la propiedad Operacion 
-        print('valor Actuador de Temperatura')
-        print(rAct_temp_Medicion)
+        print('valor Actuador de Temperatura: '+str(rAct_temp_Medicion))
         act_temp_calc = rAct_temp_Medicion
+    else:
+        print ('la lectura de actuador de temperatura devuelve error: '+str(rAct_temp.status_code))
     ################################################################
 
     #####################################################################
@@ -141,9 +153,10 @@ def executeCalc():
     if rAct_ilum.status_code == 200: # Chequeo si el resultado de la API fue correcto
         rAct_ilum_json = rAct_ilum.json() # paso el resultado a JSON
         rAct_ilum_Medicion = rAct_ilum_json['operacion'] #Obtengo el valor de la propiedad Operacion 
-        print('valor Actuador de Iluminacion')
-        print(rAct_ilum_Medicion)
+        print('valor Actuador de Iluminacion: '+str(rAct_ilum_Medicion))
         act_ilum_calc = rAct_ilum_Medicion
+    else:
+        print ('la lectura de actuador de iluminacion devuelve error: '+str(rAct_ilum.status_code))
     ################################################################
 
     #####################################################################
@@ -153,9 +166,10 @@ def executeCalc():
     if rAct_vent.status_code == 200: # Chequeo si el resultado de la API fue correcto
         rAct_vent_json = rAct_vent.json() # paso el resultado a JSON
         rAct_vent_Medicion = rAct_vent_json['operacion'] #Obtengo el valor de la propiedad Operacion 
-        print('valor Actuador de Apertura de Ventanas')
-        print(rAct_vent_Medicion)
+        print('valor Actuador de Apertura de Ventanas: '+str(rAct_vent_Medicion))
         act_vent_calc = rAct_vent_Medicion
+    else:
+        print ('la lectura de actuador de ventana devuelve error: '+str(rAct_vent.status_code))
     ################################################################
 
     #####################################################################
@@ -165,9 +179,10 @@ def executeCalc():
     if rAct_pers.status_code == 200: # Chequeo si el resultado de la API fue correcto
         rAct_pers_json = rAct_pers.json() # paso el resultado a JSON
         rAct_pers_Medicion = rAct_pers_json['operacion'] #Obtengo el valor de la propiedad Operacion 
-        print('valor Actuador de Apertura de Persianas')
-        print(rAct_pers_Medicion)
+        print('valor Actuador de Apertura de Persianas: '+str(rAct_pers_Medicion))
         act_pers_calc = rAct_pers_Medicion
+    else:
+        print ('la lectura de actuador de persiana devuelve error: '+str(rAct_pers.status_code))
     ################################################################
 
     ###################################################################
@@ -187,21 +202,17 @@ def executeCalc():
         print ('Cierro Ventana')
         act_pers_calc = "Cerrada"
         print ('Cierro Persiana')
-       
     else:
         print ('Presencia igual o mayor a 50 - el usuario esta presente')
-        print ('Temperatura interior: ')
-        print (rSensor_temp_int_Medicion)
-        print ('Temperatura configurada: ')
-        print (rUmbral_Temp_Medicion)
+        print ('Temperatura interior: ' +str(rSensor_temp_int_Medicion))
+        print ('Temperatura configurada: ' +str(rUmbral_Temp_Medicion))
         ####### Temperatura interior es mayor a la deseada #######
         if rSensor_temp_int_Medicion > rUmbral_Temp_Medicion:
             print ('Temp interior > a la deseada ')
             ##### Temperatura interior mayor a Temperatura Exterior
             if rSensor_temp_int_Medicion > rSensor_temp_ext_Medicion:
                 print ('temperatura interior es mayor a la exterior')
-                print ('estado de persiana:')
-                print (rAct_pers_Medicion)
+                print ('estado de persiana: '+str(rAct_pers_Medicion))
                 #### si la persiana esta abierta
                 if rAct_pers_Medicion == "Abierta":
                     print('la persiana esta abierta la cierro')
@@ -211,11 +222,17 @@ def executeCalc():
                         print('la ventana esta abierta la cierro')
                         act_vent_calc = "Cerrada"
                         act_temp_calc = rUmbral_Temp_Medicion - rSensor_temp_int_Medicion
+                        print ('regulo temperatura')
                         act_ilum = rSensor_ilum_int_Medicion - rUmbral_ilum_Medicion
+                        print ('regulo iluminacion')
                     else:
-                        act_vent_calc = "Abierta"
+                        print('la ventana esta cerrada la abro')
+                        act_vent_calc = "abierta"
+                        act_ilum = rSensor_ilum_int_Medicion - rUmbral_ilum_Medicion
+                        print ('regulo iluminacion')
                 #### Si la persiana esta cerrada
                 else:
+                    print('la persiana esta cerrada la abro')
                     act_pers_calc = "Abierta"
 
             ##### Temperatura interior menor o igual a Temperatura exterior 
@@ -224,39 +241,46 @@ def executeCalc():
                 act_vent_calc = "Cerrada"
                 
                 act_temp_calc = rUmbral_Temp_Medicion - rSensor_temp_int_Medicion
-                print ('corregir temperatura')
-                print (act_temp_calc)
+                print ('corregir temperatura: ' +str(act_temp_calc))
                 act_ilum_calc = rUmbral_ilum_Medicion - rSensor_ilum_int_Medicion
-                print ('corregir iluminacion')
-                print (act_temp_calc)
+                print ('corregir iluminacion: ' +str(act_temp_calc))
 
         ####### Temperatura interior es menor a la deseada #######  
         elif rSensor_temp_int_Medicion < rUmbral_Temp_Medicion:
             print ('Temp interior < a la deseada ')
             ##### Temperatura interior menor a Temperatura Exterior
             if rSensor_temp_int_Medicion < rSensor_temp_ext_Medicion:
+                print ('temp interior < temp exterior ')
                 ### si la ventana esta abierta
-                if rAct_temp_Medicion == "Abierta":
+                if rAct_vent_Medicion == "Abierta":
+                    print ('si la ventana esta abierta')
                     ### cierro la ventana
                     act_vent_calc = "Cerrada"
+                    print ('la cierro')
                 ### si la ventana esta cerrada
                 else:
+                    print ('si la ventana esta cerrada')
                     #### si la persiana esta abierta
                     if rAct_pers_Medicion == "Abierta":
-                        act_pers_calc ="Cerrada"
+                        print ('si la persiana esta abierta')
+                        act_vent_calc ="Cerrada"
+                        print ('cierro la persiana')
+                        act_temp_calc = rUmbral_Temp_Medicion - rSensor_temp_int_Medicion
+                        print ('recalculo regulacion de temperatura')
+                        act_ilum_calc = rUmbral_ilum_Medicion - rSensor_ilum_int_Medicion
+                        print ('recalculo regulación de iluminacion')
                     #### si la persiana esta cerrada
                     else:
-                        act_temp_calc = rUmbral_Temp_Medicion - rSensor_temp_int_Medicion
-                        act_ilum_calc = rUmbral_ilum_Medicion - rSensor_ilum_int_Medicion
+                        print ('si la persiana esta cerrada')
+                        act_pers_calc = "Abierta"
+                        print ('abro la persiana')
         else:
             print('temperatura interior igual a la deseada')
-            #act_temp_calc = rUmbral_Temp_Medicion - rSensor_temp_int_Medicion
-            #act_ilum_calc = rUmbral_ilum_Medicion - rSensor_ilum_int_Medicion
-
-            if rUmbral_ilum_Medicion < rSensor_ilum_int_Medicion:
-                print ('la iluminacion interior es menor a la configuracion')
-                if rSensor_ilum_int_Medicion > rSensor_ilum_ext_Medicion:
-                    print ('la iluminacion interior es menor a la exteior')
+            act_temp_calc = rUmbral_Temp_Medicion - rSensor_temp_int_Medicion
+            if rUmbral_ilum_Medicion > rSensor_ilum_int_Medicion:
+                print ('la iluminacion interior ' +str(rSensor_ilum_int_Medicion) +'  es menor a la configuracion '+str(rUmbral_ilum_Medicion))
+                if rSensor_ilum_int_Medicion < rSensor_ilum_ext_Medicion:
+                    print ('la iluminacion interior '+str(rSensor_ilum_int_Medicion) +' es menor a la exteior '+str(rSensor_ilum_ext_Medicion))
                     #### si la persiana esta abierta
                     if rAct_pers_Medicion == "Abierta":
                         print('La persiana esta abierta')
@@ -272,14 +296,15 @@ def executeCalc():
                     act_ilum_calc = rUmbral_ilum_Medicion - rSensor_ilum_int_Medicion
                     print(act_ilum_calc)
             else:
-                print ('la iluminacion interior es mayor a la configuracion')
-                if rSensor_ilum_int_Medicion > rSensor_ilum_ext_Medicion:
+                print ('la iluminacion interior ' +str(rSensor_ilum_int_Medicion) + ' es mayor a la configuracion '+str(rUmbral_ilum_Medicion))
+               
+                if rSensor_ilum_int_Medicion < rSensor_ilum_ext_Medicion:
                     print ('la iluminacion interior es menor a la exteior')
                     print('regular iluminacion')
                     act_ilum_calc = rUmbral_ilum_Medicion - rSensor_ilum_int_Medicion
                     print(act_ilum_calc)
                 else:
-                    print ('la iluminacion interior es mayor a la exteior')
+                    print ('la iluminacion interior es mayor a la exterior')
                     if rAct_pers_Medicion == "Abierta":
                         print('La persiana esta abierta, la cierro')
                         act_pers_calc ="Cerrada"
@@ -303,7 +328,7 @@ def executeCalc():
     print(rAct_ilum)
     print(rAct_ilum.content)
     if rAct_ilum.status_code == 200: # Chequeo si el resultado de la API fue correcto
-        print('post correcto')
+        print('post iluminacion correcto')
         print(act_ilum_calc)
     ################################################################
 
@@ -315,7 +340,7 @@ def executeCalc():
     print(rAct_temp)
     print(rAct_temp.content)
     if rAct_ilum.status_code == 200: # Chequeo si el resultado de la API fue correcto
-        print('post correcto')
+        print('post temperatura correcto')
         print(act_temp_calc)
     ################################################################
 
@@ -328,7 +353,7 @@ def executeCalc():
     print(rAct_vent)
     print(rAct_vent.content)
     if rAct_vent.status_code == 200: # Chequeo si el resultado de la API fue correcto
-        print('post correcto')
+        print('post ventana correcto')
         print(act_vent_calc)
     ################################################################
 
@@ -340,7 +365,7 @@ def executeCalc():
     print(rAct_pers)
     print(rAct_pers.content)
     if rAct_pers.status_code == 200: # Chequeo si el resultado de la API fue correcto
-        print('post correcto')
+        print('post persiana correcto')
         print(act_pers_calc)
     ################################################################
 
